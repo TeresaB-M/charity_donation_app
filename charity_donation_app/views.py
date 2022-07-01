b251089e056b948d4cf63677d754af19528ca6fb
TeresaB-M <teresa.buczkowska@itwl.pl>
2022-07-02 01:15:34 +0200

    add task #0 - template - views&urls

--- charity_donation_app/views.py
+++ charity_donation_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+    """Create a main form and display it on the GET method page"""
+
+    def get(self, request):
+        return render(request, "index.html")
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, "form.html")
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, "register.html")
