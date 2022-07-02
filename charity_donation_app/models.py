d9664ba2f7694b72642e276c3b1e64f8555efcea
TeresaB-M <teresa.buczkowska@itwl.pl>
2022-07-02 16:42:51 +0200

    add Model Category, migration - task #1

--- charity_donation_app/models.py
+++ charity_donation_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    @property
+    def main_name(self):
+        return "{}".format(self.name)
+
+    def __str__(self):
+        return self.main_name
