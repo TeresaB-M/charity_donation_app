157dc031851f82978e1aa363ac77833a90070e3e
TeresaB-M <teresa.buczkowska@itwl.pl>
2022-07-02 17:11:54 +0200

    add Model Institution for task #1

--- charity_donation_app/models.py
+++ charity_donation_app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+TYPE_INSTITUTION = (
+    (1, "Fundacja"),
+    (2, "Organizacja pozarządowa"),
+    (3, "Zbiórka lokalna"),
+)
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
@@ -7,6 +12,19 @@ class Category(models.Model):
     @property
     def main_name(self):
         return "{}".format(self.name)
+
+    def __str__(self):
+        return self.main_name
+
+class Institution(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    type = models.IntegerField(choices=TYPE_INSTITUTION, default=1)
+    categories = models.ManyToManyField(Category)
+
+    @property
+    def main_name(self):
+        return "{} {}".format(self.name, self.description, self.type, self.categories)
 
     def __str__(self):
         return self.main_name
