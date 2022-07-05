0727a04bd893abafa149971ae423a678344cd34d
TeresaB-M <teresa.buczkowska@itwl.pl>
2022-07-05 20:01:24 +0200

    #2 dynamic load page with institution

--- charity_donation_app/views.py
+++ charity_donation_app/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 
-from charity_donation_app.models import Donation, Institution
+from charity_donation_app.models import Donation, Institution, Category
 from django.db.models import Sum
 
 
@@ -10,23 +10,32 @@ class LandingPageView(View):
     """Create a main form and display it on the GET method page"""
 
     def get(self, request):
-        institution = Institution.objects.order_by("id")
-        paginator = Paginator(institution, 5)
-        page = request.GET.get('page')
-        institutions = paginator.get_page(page)
         total = Donation.objects.aggregate(Sum('quantity'))
         counter_institution = Donation.objects.values('institution').distinct().count()
         fundation = Institution.objects.filter(type=1).order_by('name')
         organization = Institution.objects.filter(type=2).order_by('name')
         local = Institution.objects.filter(type=3).order_by('name')
-        return render(request, "index.html", context={"institutions": institutions,
-                                                      "range": range(1, paginator.num_pages +1),
-                                                      "total": total,
+
+        paginator1 = Paginator(fundation, 3)
+        page_1 = request.GET.get('page')
+
+        fundations = paginator1.get_page(page_1)
+        paginator2 = Paginator(organization, 3)
+        page_2 = request.GET.get('page')
+        organizations = paginator2.get_page(page_2)
+        paginator3 = Paginator(local, 3)
+        page_3 = request.GET.get('page')
+        locs = paginator3.get_page(page_3)
+        categories = Category.objects.all()
+        return render(request, "index.html", context={"total": total,
                                                       "counter_institution": counter_institution,
                                                       "fundation": fundation,
                                                       "organization": organization,
                                                       "local": local,
-                                                      })
+                                                      "fundations": fundations,
+                                                      "organizations": organizations,
+                                                      "locs": locs,
+                                                      "categories": categories})
 
 
 class AddDonationView(View):
@@ -42,3 +51,6 @@ class LoginView(View):
 class RegisterView(View):
     def get(self, request):
         return render(request, "register.html")
+
+    def post(self, request):
+        name = request.POST.get('name')
