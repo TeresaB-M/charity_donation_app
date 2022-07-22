e040a420aa584e5092925f8a4c14040c9e21c95b
TeresaB-M <teresa.buczkowska@itwl.pl>
2022-07-22 22:28:27 +0200

    #6 create admin panel + attitiude form

--- charity_donation_app/admin.py
+++ charity_donation_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from charity_donation_app.models import Category, Institution, Donation
 
+
 admin.site.register(Category)
 admin.site.register(Institution)
 admin.site.register(Donation)
